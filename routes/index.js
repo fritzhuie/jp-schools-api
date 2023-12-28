992527c73f5824c290dded0c86d275147e29eea0
fritzhuie <fritzhuie@gmail.com>
2023-12-28 13:30:57 -0500

    add new routes

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/schools', async function(req, res) {
             response
         })
     }catch(error) {
-        res.status(418).json({
+        res.status(500).json({
             message: message,
             message: `There was an error getting schools`
         })
@@ -26,9 +26,42 @@ router.post('/schools', async function(req, res) {
         })
     } catch(error) {
         const message = error.message
-        res.status(418).json({
+        res.status(500).json({
             message: message,
             message: `There was an error creating a school`
+        })
+    }
+})
+
+router.put('/:sid', async function(req, res) {
+    try {
+        const payload = req.body
+        const id = req.params.sid
+        const updatedSchool = await updateSchool(id, payload)
+        res.status(200).json({
+            updatedSchool
+        })
+    } catch (error) {
+        const message = error.message
+        res.status(500).json({
+            message: message,
+            message: `There was an error updating a school`
+        })
+    }
+})
+
+router.delete('/:sid', async function(req, res) {
+    try {
+        const id = req.params.sid
+        const response = await deleteSchool(id)
+        res.status(200).json({
+            response
+        })
+    } catch(error) {
+        const message = error.message
+        res.status(418).json({
+            message: message,
+            message: `There was an error deleting a movie`
         })
     }
 })
